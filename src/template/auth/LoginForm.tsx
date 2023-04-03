7b2660d9259aef1ff55af0fdcb4293a6670d477e
IMPWNG <matisse.ach+github@gmail.com>
2023-04-03 16:07:00 -0400

    f

--- src/template/auth/LoginForm.tsx
+++ src/template/auth/LoginForm.tsx
@@ -1,12 +1,11 @@
+/* eslint-disable no-console */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Auth } from '@supabase/auth-ui-react';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 import { FullCenterSection } from '@/layout/FullCenterSection';
-import { getURL } from '@/utils/url';
 
 // eslint-disable-next-line consistent-return
 const LoginForm = () => {
@@ -20,16 +19,52 @@ const LoginForm = () => {
     }
   }, [user]);
 
+  const handleLogInWithGoogle = async () => {
+    const { data, error } = await supabaseClient.auth.signInWithOAuth({
+      provider: 'google',
+    });
+    if (error) {
+      console.log('Error logging in with Google:', error.message);
+
+      return;
+    }
+    console.log('data', data);
+  };
+
   if (!user) {
     return (
       <FullCenterSection title="Sign in to your account">
-        <Auth
-          supabaseClient={supabaseClient}
-          providers={['github']}
-          redirectTo={getURL()}
-          magicLink={true}
-          theme="dark"
-        />
+        <div className="align-center my-4 flex items-center justify-center text-center">
+          <button
+            onClick={handleLogInWithGoogle}
+            className="google-signin-button"
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              background: '#4285F4',
+              color: '#fff',
+              borderRadius: '4px',
+              border: '1px solid #4285F4',
+              fontSize: '14px',
+              fontWeight: 'bold',
+              fontFamily: 'Roboto, Arial, sans-serif',
+              cursor: 'pointer',
+              padding: '8px 24px',
+            }}
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+              alt="Google logo"
+              style={{
+                width: '18px',
+                height: '18px',
+                marginRight: '8px',
+              }}
+            />
+            Sign in with Google
+          </button>
+        </div>
       </FullCenterSection>
     );
   }
