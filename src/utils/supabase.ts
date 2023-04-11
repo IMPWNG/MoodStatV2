e6cb94debf79b1af14ce92e66f005ea7693a1525
IMPWNG <matisse.ach+github@gmail.com>
2023-04-11 15:07:01 -0400

    comit

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,8 +1,8 @@
 // utils/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.DB_SUPABASE_URL as string;
-const SUPABASE_ANON_KEY = process.env.DB_SUPABASE_ANON_KEY as string;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
